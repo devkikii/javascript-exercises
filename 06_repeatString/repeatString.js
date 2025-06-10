169aecd0e6cc6ac9c922bce0bcaea28224617443
Kristijan Maric <kiki.maric78@gmail.com>
2025-06-10 22:18:40 +0200

    Complete 'repeatString' exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = ""
+    if (num < 0) {
+        return ("ERROR")
+    } 
+    for (let i = 0; i < num; i ++)
+        result += string
+    return result
 };
+
+console.log(repeatString("hey", 10000))
 
 // Do not edit below this line
 module.exports = repeatString;
