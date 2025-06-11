a26d019cfdd0ae80e2f95c398a103a583607dc82
Kristijan Maric <kiki.maric78@gmail.com>
2025-06-11 22:21:05 +0200

    Complete 'sumAll' exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let finalSum = 0
+    if (!Number.isInteger(a) || (!Number.isInteger(b)))
+        return ("ERROR")
 
+    if (a > b) {
+      let temp = a;
+      a = b;
+      b = temp;
+    }
+
+    if (a < 0 || b < 0)
+        return ("ERROR")
+    
+    for (let i = a; i <= b; i++) {
+        finalSum = finalSum + i
+    } 
+    return finalSum
 };
+console.log(sumAll(5, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
