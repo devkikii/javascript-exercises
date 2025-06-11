42d6e7fc0d8ee84aa38c9c0300264629fe524418
Kristijan Maric <kiki.maric78@gmail.com>
2025-06-11 22:50:58 +0200

    Complete 'tempConversion' exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  return parseFloat(((temperature - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return parseFloat(((temperature * 9 / 5) + 32).toFixed(1));
 };
+console.log(convertToCelsius(100))
+console.log(convertToFahrenheit(37))
 
 // Do not edit below this line
 module.exports = {
