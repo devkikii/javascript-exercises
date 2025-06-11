a62c7e10f6f59ade9c63a32ea63531876bd52e2e
Kristijan Maric <kiki.maric78@gmail.com>
2025-06-11 21:35:18 +0200

    Complete 'reverseString' exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string
+    .split("")
+    .reverse()
+    .join("")
 };
+console.log(reverseString("banana ananas"))
 
 // Do not edit below this line
 module.exports = reverseString;
